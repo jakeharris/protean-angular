18c063711cb840171bbb75f475c5c899d2358b37
Jake Harris <milieu@javakat.io>
2019-05-08 10:54:25 -0500

    Add type guard function (is there any easier way?)

--- src/app/models/pokemon-type.ts
+++ src/app/models/pokemon-type.ts
@@ -24,3 +24,26 @@ export interface PokemonTypeDefensiveEffectiveness {
   resistances: PokemonType[];
   immunities: PokemonType[];
 }
+
+export function isPokemonType(typeName: string): typeName is PokemonType {
+  return (
+    typeName === 'normal' ||
+    typeName === 'fire' ||
+    typeName === 'water' ||
+    typeName === 'grass' ||
+    typeName === 'electric' ||
+    typeName === 'ground' ||
+    typeName === 'rock' ||
+    typeName === 'ice' ||
+    typeName === 'steel' ||
+    typeName === 'fighting' ||
+    typeName === 'psychic' ||
+    typeName === 'dark' ||
+    typeName === 'ghost' ||
+    typeName === 'fairy' ||
+    typeName === 'dragon' ||
+    typeName === 'flying' ||
+    typeName === 'poison' ||
+    typeName === 'bug'
+  );
+}
