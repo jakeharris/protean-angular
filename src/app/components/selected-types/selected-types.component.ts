c447558f00a297b43ff4770be06cf2e1029d357b
Jake Harris <jake.harris@synapsewireless.com>
2019-05-09 14:16:13 -0500

    Style selected-types

--- src/app/components/selected-types/selected-types.component.ts
+++ src/app/components/selected-types/selected-types.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { PokemonType } from 'src/app/models/pokemon-type';
 
 @Component({
   selector: 'app-selected-types',
   template: `
-    <div *ngFor="let type of types" [attr.class]="type">
+    <div *ngFor="let type of types" [attr.class]="'type-button ' + type">
       <span class="remove" (click)="deselectType.emit(type)">x</span>
       {{ type }}
     </div>
   `,
-  styles: []
+  styles: [
+    `
+      :host {
+        display: flex;
+        justify-content: space-around;
+        width: 100%;
+        padding-top: 3rem;
+      }
+      .type-button {
+        padding: 1.5rem;
+        margin: 0 1.5em;
+        font-size: 1.5rem;
+        flex-basis: 25%;
+        text-transform: capitalize;
+        text-align: center;
+        position: relative;
+      }
+      .type-button .remove {
+        font-size: 1rem;
+        font-family: sans-serif;
+        text-align: center;
+        cursor: pointer;
+        padding: 10px 12px;
+        position: absolute;
+        top: 0;
+        right: 0;
+      }
+      .type-button .remove:hover {
+        background: white;
+      }
+    `
+  ]
 })
 export class SelectedTypesComponent {
   @Input() types: PokemonType[];
