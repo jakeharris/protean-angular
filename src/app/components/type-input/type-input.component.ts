f35bcd68cfd2ed7fef2ce7acdc7d56a060950ba5
Jake Harris <jake.harris@synapsewireless.com>
2019-05-09 11:49:04 -0500

    Actually reset text input value

--- src/app/components/type-input/type-input.component.ts
+++ src/app/components/type-input/type-input.component.ts
@@ -1,32 +1,44 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 import { PokemonType, isPokemonType } from 'src/app/models/pokemon-type';
-import { repeat, takeUntil, tap, filter, map } from 'rxjs/operators';
+import { tap, filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-type-input',
   template: `
     <input
       type="text"
-      (input)="input$.next($event.target.value)"
-      [value]="typeElementValue$ | async"
+      placeholder="Type some Pokemon types here!"
+      [formControl]="input"
     />
   `,
-  styles: [``]
+  styles: [
+    `
+      input {
+        width: 100%;
+        height: 200px;
+        border: none;
+        outline: none;
+        font-size: 48px;
+        padding: 48px;
+      }
+    `
+  ]
 })
 export class TypeInputComponent implements OnInit {
-  input$ = new BehaviorSubject<string>('');
+  input = new FormControl();
   typeElementValue$ = new BehaviorSubject<string>('');
 
   @Output() selectType = new EventEmitter<PokemonType>();
 
   ngOnInit() {
-    this.input$
+    this.input.valueChanges
       .pipe(
         map(partialType => partialType.toLowerCase()),
         filter(partialType => isPokemonType(partialType)),
         tap((selectedType: PokemonType) => this.selectType.emit(selectedType)),
-        tap(_ => this.typeElementValue$.next(''))
+        tap(_ => this.input.setValue(''))
       )
       .subscribe();
   }
