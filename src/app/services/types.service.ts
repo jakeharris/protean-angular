9612a47499702a59045f257e944affd9396594f5
Jake Harris <jake.harris@synapsewireless.com>
2019-06-14 10:34:57 -0500

    Make path relative to avoid CORS issues

--- src/app/services/types.service.ts
+++ src/app/services/types.service.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PokemonTypeDefensiveEffectiveness } from '../models/pokemon-type';
-import { environment } from 'src/environments/environment';
 
-const url = environment.apiUrl + 'assets/types.json';
+const url = 'assets/types.json';
 
 @Injectable()
 export class TypesService {
